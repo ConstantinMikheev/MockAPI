ddeeea99da957e862b44cad3445d594492757e59
Михеев Константин Александрович <mikheev.k@skbkontur.ru>
2023-07-09 19:13:27 +0500

    Добавил в модель DataContent

---
+++ Model/DataContent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public class DataContent: IEnumerable
+    {
+        private Dictionary<string, List<DataMethodContent>> content;
+
+        #region Constructors
+        public DataContent()
+        {
+            content = new Dictionary<string, List<DataMethodContent>>();
+        }
+        #endregion
+
+        #region Interface methods
+        public IEnumerator GetEnumerator()
+        {
+            foreach(var item in content)
+                yield return item;
+        }
+        #endregion
+
+        #region Other methods
+        public void AddDataMethodContent(string requestString)
+        {
+            content.Add(requestString, new List<DataMethodContent>());
+        }
+
+        public void AddDataMethodContent(string requestString, APIMethods methods)
+        {
+            var dataMethods = new List<DataMethodContent>();
+            foreach (APIMethod method in methods)
+                dataMethods.Add(new DataMethodContent(requestString, method));
+            content.Add(requestString, dataMethods);
+        }
+
+        public DataMethodContent[] GetDataMethodContents(string requestString)
+        {
+            if (!content.ContainsKey(requestString))
+                return null;
+
+            return content[requestString].ToArray();
+        }
+
+        public List<DataMethodContent>[] PrepareForSerialization()
+        {
+            var result = new List<List<DataMethodContent>>();
+            foreach (var item in content.Values)
+                result.Add(item);
+
+            return result.ToArray();
+        }
+        #endregion
+    }
+}
---
+++ Model/DataMethodContent.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Model
+{
+    /// <summary>
+    /// Данные, полученные по методу
+    /// </summary>
+    public class DataMethodContent
+    {
+        /// <summary>
+        /// Запрашиваемые данные
+        /// </summary>
+        public string RequestData { get; set; }
+        /// <summary>
+        /// Метод API
+        /// </summary>
+        public APIMethod ApiMethod { get; set; }
+        /// <summary>
+        /// Содержимое в текстовом виде
+        /// </summary>
+        public string Content { get; set; }
+
+        public DataMethodContent(string requestData, APIMethod apiMethod)
+        {
+            RequestData = requestData;
+            ApiMethod = apiMethod;
+            Content = string.Empty;
+        }
+    }
+}
