aa42365c039a36facdb3a22178d234b61c05b630
Михеев Константин Александрович <mikheev.k@skbkontur.ru>
2023-07-09 10:49:37 +0500

    Модели: добавил методы API

---
+++ Model/APIMethod.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Models
+{
+    /// <summary>
+    /// Метод API
+    /// </summary>
+    public class APIMethod
+    {
+        /// <summary>
+        /// Текстовый идентификатор метода
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Загружать данные по методу с сервера API
+        /// </summary>
+        public bool Enabled { get; set; }
+        /// <summary>
+        /// Строка получения данных из API
+        /// </summary>
+        public string ResourceString { get; set; }
+
+        public APIMethod(string name)
+        {
+            Name = name;
+            Enabled = false;
+            ResourceString = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Model/APIMethods.cs
@@ -0,0 +1,59 @@
+﻿using Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Model
+{
+    public class APIMethods: IEnumerable
+    {
+        private Dictionary<string, APIMethod> methods;
+
+        #region Constructors
+        public APIMethods()
+        {
+            methods = new Dictionary<string, APIMethod>();
+        }
+
+        public APIMethods(APIMethod[] arrayOfMethods)
+        {
+            methods = new Dictionary<string, APIMethod>();
+
+            foreach(var item in arrayOfMethods)
+                methods[item.Name] = item;
+        }
+        #endregion
+
+        #region Interface realisation
+        public IEnumerator GetEnumerator()
+        {
+            foreach(var method in methods.Values)
+                yield return method;
+        }
+        #endregion
+
+        #region Other methods
+        public void Add(string method)
+        {
+            var newMethod = new APIMethod(method);
+            Add(newMethod);
+        }
+
+        public void Add(APIMethod method)
+        {
+            var name = method.Name.ToLower();
+
+            if (methods.ContainsKey(name))
+                throw new Exception($"Метод с именем {name} уже существует");
+
+            methods.Add(name, method);
+        }
+
+        public APIMethod[] PrepareForSerialization()
+        {
+            return methods.Values.ToArray();
+        }
+        #endregion
+    }
+}
